d6f7a37f961eabca24192f8bfdb161838c591fbe
Zachary D. Shaw <zachary@icosystem.com>
2013-07-15 19:17:11 -0400

    adding recipes

--- angularjs/app/js/controllers.js
+++ angularjs/app/js/controllers.js
@@ -6,7 +6,12 @@ function RecipeCtrl($scope) {
 	$scope.recipes = [
 		{name:"ricenbeans",ingredients: ["beans","rice"], instructions: ["mix","boil"]},
 		{name:"pasta",ingredients: ["pasta","sauce"], instructions: ["boid","mix"]},
-	]
+	];
+
+	$scope.addRecipe = function() {
+		$scope.recipes.push({name:$scope.newRecipeName});
+		$scope.newRecipeName = '';
+	};
 
 
 }

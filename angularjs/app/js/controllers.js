c0f642cbe75cad0f45bd1fbf393bbef6d6f2830f
Zachary D. Shaw <zachary@icosystem.com>
2013-07-15 19:09:31 -0400

    now showing two recipes

--- angularjs/app/js/controllers.js
+++ angularjs/app/js/controllers.js
@@ -1,3 +1,13 @@
 'use strict';
 
 /* Controllers */
+function RecipeCtrl($scope) {
+	$scope.bob = "Robert";
+
+	$scope.recipes = [
+		{name:"ricenbeans",ingredients: ["beans","rice"], instructions: ["mix","boil"]},
+		{name:"pasta",ingredients: ["pasta","sauce"], instructions: ["boid","mix"]},
+	]
+
+
+}
